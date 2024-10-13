f351206544edaec701a9cbd91aaf10285662045b
kuzgunT <darkarastirma40@outlook.com>
2024-10-13 15:51:47 +0300

    Form1.Designer.cs

---
+++ CatizenCrypto-Bot/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace Hamster_Key_Generator
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.comboBoxGames = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBarMain = new System.Windows.Forms.ProgressBar();
+            this.numericUpDownCount = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBoxLogs = new System.Windows.Forms.RichTextBox();
+            this.buttonCopyAll = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDownMinimumDelay = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxKeys = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.labelKeys = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labelRequest = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labelProccess = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBoxSettings = new System.Windows.Forms.GroupBox();
+            this.checkBoxProccessRandomDelay = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownProccess = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinimumDelay)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.groupBoxSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownProccess)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGenerate.Location = new System.Drawing.Point(496, 26);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // comboBoxGames
+            // 
+            this.comboBoxGames.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxGames.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxGames.FormattingEnabled = true;
+            this.comboBoxGames.Location = new System.Drawing.Point(12, 25);
+            this.comboBoxGames.Name = "comboBoxGames";
+            this.comboBoxGames.Size = new System.Drawing.Size(326, 21);
+            this.comboBoxGames.TabIndex = 1;
+            this.comboBoxGames.SelectedIndexChanged += new System.EventHandler(this.comboBoxGames_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Game";
+            // 
+            // progressBarMain
+            // 
+            this.progressBarMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarMain.Location = new System.Drawing.Point(12, 466);
+            this.progressBarMain.Name = "progressBarMain";
+            this.progressBarMain.Size = new System.Drawing.Size(559, 16);
+            this.progressBarMain.TabIndex = 3;
+            // 
+            // numericUpDownCount
+            // 
+            this.numericUpDownCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownCount.Location = new System.Drawing.Point(344, 25);
+            this.numericUpDownCount.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownCount.Name = "numericUpDownCount";
+            this.numericUpDownCount.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDownCount.TabIndex = 4;
+            this.numericUpDownCount.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Count";
+            // 
+            // richTextBoxLogs
+            // 
+            this.richTextBoxLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxLogs.BackColor = System.Drawing.Color.Black;
+            this.richTextBoxLogs.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxLogs.ForeColor = System.Drawing.Color.White;
+            this.richTextBoxLogs.Location = new System.Drawing.Point(12, 52);
+            this.richTextBoxLogs.Name = "richTextBoxLogs";
+            this.richTextBoxLogs.ReadOnly = true;
+            this.richTextBoxLogs.Size = new System.Drawing.Size(559, 291);
+            this.richTextBoxLogs.TabIndex = 6;
+            this.richTextBoxLogs.Text = "";
+            this.richTextBoxLogs.WordWrap = false;
+            // 
+            // buttonCopyAll
+            // 
+            this.buttonCopyAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCopyAll.Location = new System.Drawing.Point(478, 48);
+            this.buttonCopyAll.Name = "buttonCopyAll";
+            this.buttonCopyAll.Size = new System.Drawing.Size(75, 23);
+            this.buttonCopyAll.TabIndex = 8;
+            this.buttonCopyAll.Text = "Copy All";
+            this.buttonCopyAll.UseVisualStyleBackColor = true;
+            this.buttonCopyAll.Click += new System.EventHandler(this.buttonCopyAll_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCopy.Location = new System.Drawing.Point(478, 19);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(75, 23);
+            this.buttonCopy.TabIndex = 9;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDownMinimumDelay
+            // 
+            this.numericUpDownMinimumDelay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownMinimumDelay.Location = new System.Drawing.Point(352, 35);
+            this.numericUpDownMinimumDelay.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDownMinimumDelay.Name = "numericUpDownMinimumDelay";
+            this.numericUpDownMinimumDelay.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownMinimumDelay.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "MinimumDelay";
+            // 
+            // listBoxKeys
+            // 
+            this.listBoxKeys.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxKeys.BackColor = System.Drawing.Color.Black;
+            this.listBoxKeys.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxKeys.ForeColor = System.Drawing.Color.Lime;
+            this.listBoxKeys.FormattingEnabled = true;
+            this.listBoxKeys.Location = new System.Drawing.Point(6, 19);
+            this.listBoxKeys.Name = "listBoxKeys";
+            this.listBoxKeys.Size = new System.Drawing.Size(337, 82);
+            this.listBoxKeys.TabIndex = 7;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelKeys,
+            this.labelRequest,
+            this.labelProccess});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 485);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(583, 22);
+            this.statusStrip1.TabIndex = 14;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // labelKeys
+            // 
+            this.labelKeys.Name = "labelKeys";
+            this.labelKeys.Size = new System.Drawing.Size(26, 17);
+            this.labelKeys.Text = "Key";
+            // 
+            // labelRequest
+            // 
+            this.labelRequest.Name = "labelRequest";
+            this.labelRequest.Size = new System.Drawing.Size(49, 17);
+            this.labelRequest.Text = "Request";
+            // 
+            // labelProccess
+            // 
+            this.labelProccess.Name = "labelProccess";
+            this.labelProccess.Size = new System.Drawing.Size(53, 17);
+            this.labelProccess.Text = "Proccess";
+            // 
+            // groupBoxSettings
+            // 
+            this.groupBoxSettings.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxSettings.Controls.Add(this.checkBoxProccessRandomDelay);
+            this.groupBoxSettings.Controls.Add(this.listBoxKeys);
+            this.groupBoxSettings.Controls.Add(this.buttonCopy);
+            this.groupBoxSettings.Controls.Add(this.numericUpDownMinimumDelay);
+            this.groupBoxSettings.Controls.Add(this.label3);
+            this.groupBoxSettings.Controls.Add(this.buttonCopyAll);
+            this.groupBoxSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBoxSettings.Location = new System.Drawing.Point(12, 349);
+            this.groupBoxSettings.Name = "groupBoxSettings";
+            this.groupBoxSettings.Size = new System.Drawing.Size(559, 111);
+            this.groupBoxSettings.TabIndex = 15;
+            this.groupBoxSettings.TabStop = false;
+            this.groupBoxSettings.Text = "Keys and settings";
+            // 
+            // checkBoxProccessRandomDelay
+            // 
+            this.checkBoxProccessRandomDelay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxProccessRandomDelay.AutoSize = true;
+            this.checkBoxProccessRandomDelay.Location = new System.Drawing.Point(352, 84);
+            this.checkBoxProccessRandomDelay.Name = "checkBoxProccessRandomDelay";
+            this.checkBoxProccessRandomDelay.Size = new System.Drawing.Size(182, 17);
+            this.checkBoxProccessRandomDelay.TabIndex = 12;
+            this.checkBoxProccessRandomDelay.Text = "Start proccess with random delay";
+            this.checkBoxProccessRandomDelay.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(417, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Proccess";
+            // 
+            // numericUpDownProccess
+            // 
+            this.numericUpDownProccess.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownProccess.Location = new System.Drawing.Point(420, 25);
+            this.numericUpDownProccess.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownProccess.Name = "numericUpDownProccess";
+            this.numericUpDownProccess.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDownProccess.TabIndex = 16;
+            this.numericUpDownProccess.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 507);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDownProccess);
+            this.Controls.Add(this.groupBoxSettings);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBoxLogs);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownCount);
+            this.Controls.Add(this.progressBarMain);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxGames);
+            this.Controls.Add(this.buttonGenerate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Hamster Key Generator v1.1.0";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinimumDelay)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBoxSettings.ResumeLayout(false);
+            this.groupBoxSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownProccess)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.ComboBox comboBoxGames;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBarMain;
+        private System.Windows.Forms.NumericUpDown numericUpDownCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBoxLogs;
+        private System.Windows.Forms.Button buttonCopyAll;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinimumDelay;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxKeys;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labelKeys;
+        private System.Windows.Forms.ToolStripStatusLabel labelRequest;
+        private System.Windows.Forms.ToolStripStatusLabel labelProccess;
+        private System.Windows.Forms.GroupBox groupBoxSettings;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownProccess;
+        private System.Windows.Forms.CheckBox checkBoxProccessRandomDelay;
+    }
+}
+
